df2f1894c302dd5a4dd8cc2d1f5fe14bac8251f2
wangxinxin <510341830@qq.com>
2015-11-19 10:56:38 +0800

    Initial Commit

---
+++ WebViewPJ/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WebViewPJ
+//
+//  Created by wangxinxin on 15/11/19.
+//  Copyright (c) 2015年 wangxinxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WebViewPJ/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WebViewPJ
+//
+//  Created by wangxinxin on 15/11/19.
+//  Copyright (c) 2015年 wangxinxin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
